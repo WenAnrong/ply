d875b75cebf1ff032e04882771f4a6b599445ab2
war <warhavefun@163.com>
2026-09-02 22:56:34 +0800

    添加docker管理界面导航栏

--- views/docker.py
+++ views/docker.py
@@ -12,4 +12,22 @@ docker_bp = Blueprint("docker", __name__)
 @docker_bp.route("/docker")
 @login_required
 def index():
-    return render_template("docker.html")
+    return render_template("docker.html", active_tab="services")
+
+
+@docker_bp.route("/docker/services")
+@login_required
+def services():
+    return render_template("docker.html", active_tab="services")
+
+
+@docker_bp.route("/docker/images")
+@login_required
+def images():
+    return render_template("docker.html", active_tab="images")
+
+
+@docker_bp.route("/docker/settings")
+@login_required
+def settings():
+    return render_template("docker.html", active_tab="settings")
